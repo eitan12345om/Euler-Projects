12b587852d4587ecbc4c2fb48844a73ebf7e802c
eitan12345om <eitan.simler@gmail.com>
2014-09-07 11:40:42 -0700

    no need for input

--- Euler48.py
+++ Euler48.py
@@ -5,7 +5,7 @@ Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
 
 y = 0
 
-for x in range(1,int(input("range: "))): #should be 1001 for problem
+for x in range(1,1001)
 	x = x**x
 	y += x
 
